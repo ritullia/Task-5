5f02027b864e7e77cf9264d0645891f1b9a3ff04
ritullia <brigrita@gmail.com>
2023-02-15 23:01:13 +0200

    function filterDogOwers commit

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+function filterDogOwers(users) {
+  for (let i = 0; i < users.length; i++) {
+
+    if (users[i].hasDog === true) {
+      console.log(users[i])
+    }
+  }
+}
+
+filterDogOwers(users)
